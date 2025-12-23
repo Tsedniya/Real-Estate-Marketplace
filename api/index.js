e43c56b50ada74e01244301d176d3f97d8b5e902
Tsedniya <ktsedniya@gmail.com>
2025-12-23 21:15:45 +0300

    backend:mongoos

--- api/index.js
+++ api/index.js
@@ -4,7 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-mongoose.connect(process.env.MONGO);
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to MongoDB')
+}).catch((err)=>{
+    console.log(err)
+});
+
 const app = express();
 
 app.listen(3000,()=>{

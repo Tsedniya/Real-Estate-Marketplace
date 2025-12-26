d1285ae1c5eb4abb4fa444cb7eb46a89e1de9ad1
Tsedniya <ktsedniya@gmail.com>
2025-12-27 00:42:13 +0300

    frontend:signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
-       <h1 className='text-3xl text-center font-semibold text-white'>SignUp</h1>
-      <form className='flex flex-col items-center justify-center gap-4'> 
-        <input className='border p-3 rounded-lg text-white' id='username' type="text" placeholder='Username'/>
-        <input className='border p-3 rounded-lg text-white' type="email" placeholder='Email'/>
-        <input className='border p-3 rounded-lg text-white' type="password" placeholder='Password'/>
-        <button>SIGN UP</button>
-        <button>CONTINUE WITH GOOGLE</button>
+    <div className='p-3 max-w-lg mx-auto'>
+       <h1 className='my-7 text-3xl text-center font-semibold text-white'>SignUp</h1>
+      <form className='flex flex-col gap-4'> 
+        <input className='border p-3 rounded-lg text-white w-full' id='username' type="text" placeholder='Username'/>
+        <input className='border p-3 rounded-lg text-white w-full' type="email" placeholder='Email'/>
+        <input className='border p-3 rounded-lg text-white w-full' type="password" placeholder='Password'/>
+        <button className="border p-3 rounded-lg w-full bg-white text-[#022222] hover:bg-[#022222] hover:text-white uppercase">SIGN UP</button>
+        <button className='className="border p-3 rounded-lg uppercase bg-white text-[#022222] hover:bg-[#022222] hover:text-white w-full'>CONTINUE WITH GOOGLE</button>
       </form>
+      <div className='flex gap-2 mt-5'>
+        <p className='text-white'>Have an account?</p>
+        <Link to={"/signin"}>
+            <span className='text-sky-300'>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }

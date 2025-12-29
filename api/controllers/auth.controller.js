b83e614fb2d0fefd9b4072f130d34a60d6e626f2
Tsedniya <ktsedniya@gmail.com>
2025-12-29 12:22:16 +0300

    backend:jwt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken'
+import {errorHandler} from '../utils/error.js'
 
 export const signup = async(req, res, next) => {
   const {username, email, password} = req.body;
@@ -20,8 +22,12 @@ export const signin = async(req, res, next) => {
     const validUser = await User.findOne({email})
     if(!validUser) return next(errorHandler(404,'User not found'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if(!validPassword) return next(errorHandler(401,'Invalid password'));
-
+    if(!validPassword) return next(errorHandler(401,'Wrong credentials'));
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    res
+    .cookie('access_token', token, {httpOnly: true})
+    .status(200)
+    .json(validUser)
   }catch(error){
     next(error)
   }

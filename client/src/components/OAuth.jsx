bd1407770c84f4370b32d72553044312162a3991
Tsedniya <ktsedniya@gmail.com>
2026-02-02 17:14:55 +0300

    backend:googleouth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,35 +1,50 @@
 import React from 'react';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 const OAuth = () => {
-    const handleGoogleClick = async()=>{
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();   
+  const navigate = useNavigate();  
 
-     try{
-        const provider = new GoogleAuthProvider()
-        const auth = getAuth(app)
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-        const result = await signInWithPopup(auth,provider)
+      const result = await signInWithPopup(auth, provider);
 
-        const res = await fetch('/api/auth/google',{
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({name:result.user.displayName, email:result.user.email,photo:result.user.photoURL}),
-        });
-        const data = await res.json();
-        dispatch(signInSuccess(data));
-        } catch(error){
-            console.log('could not sign in with google', error);
-        }
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL
+        }),
+      });
+
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('could not sign in with google', error);
     }
+  };
+
   return (
-    <button onClick={handleGoogleClick} type='button' className="border p-3 rounded-lg w-full bg-white text-[#022222] hover:bg-[#022222] hover:text-white uppercase opacity-95">CONTINUE WITH GOOGLE</button>
-  )
-}
+    <button
+      onClick={handleGoogleClick}
+      type="button"
+      className="border p-3 rounded-lg w-full bg-white text-[#022222] hover:bg-[#022222] hover:text-white uppercase opacity-95"
+    >
+      CONTINUE WITH GOOGLE
+    </button>
+  );
+};
 
 export default OAuth;

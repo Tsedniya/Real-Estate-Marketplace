b8271c6f64345d74ffb00fe47a0342807766b121
Tsedniya <ktsedniya@gmail.com>
2026-07-23 23:44:47 +0300

    frontend:searchpart

---
+++ client/src/components/ListingItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ListingItem = ({listing}) => {
+  return (
+    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden'>
+       <Link to={`/listing/${listing.id}`}>
+       <img src={listing.imageUrls[0]} 
+       alt= 'listing cover'
+        className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'/>
+    
+    <div className='p-3'>
+        <p className='truncate text-lg font-semibold text-slate-700'>{listing.name}</p>
+    </div>
+    </Link>
+    </div>
+  )
+}
+
+export default ListingItem
